f7eabd2798f399bb72840e42d9cee382cfe8cf6a
Archy de Berker <archy.deberker@gmail.com>
2019-10-06 15:20:01 -0400

    Fix Procfile

--- app.py
+++ app.py
@@ -15,9 +15,8 @@ def create_app():
 
     return app
 
+app = create_app()
 
-if __name__ == "__main__":
-    app = create_app()
-    app.run(
+if __name__ == "__main__":    app.run(
         debug=True, host="127.0.0.1", port=5000
     )  # run app in debug mode on port 5000
